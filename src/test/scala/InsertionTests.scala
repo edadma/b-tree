a60911e10be16e29c2a4a819563720c24ff43be4
Edward A Maxedon, Sr <edadma@gmail.com>
2016-09-17 21:52:40 -0400

    merge all insertion tests into one file

--- src/test/scala/InsertionTests.scala
+++ src/test/scala/InsertionTests.scala
@@ -5,7 +5,7 @@ import prop.PropertyChecks
 import org.scalatest.prop.TableDrivenPropertyChecks._
 
 
-class FileTests extends FreeSpec with PropertyChecks with Matchers {
+class InsertionTests extends FreeSpec with PropertyChecks with Matchers {
 	
 	val order3 =
 		Table(
@@ -13,6 +13,13 @@ class FileTests extends FreeSpec with PropertyChecks with Matchers {
 			//----------------                               -------
 			(() => new MemoryBPlusTree[String, Any]( 3 ),    "in memory"),
 			(() => new FileBPlusTree[String, Any]( "btree", 3, true ),    "on disk")
+	 		)
+	val order4 =
+		Table(
+			("object generator", 													"storage"),
+			//----------------                               -------
+			(() => new MemoryBPlusTree[String, Any]( 4 ),    "in memory"),
+			(() => new FileBPlusTree[String, Any]( "btree", 4, true ),    "on disk")
 	 		)
 	
 	forAll (order3) { (gen, storage) =>
@@ -99,54 +106,59 @@ class FileTests extends FreeSpec with PropertyChecks with Matchers {
 		}
 	}
 	
-	"ascending insertion (order 4)" in {
-		val tree = new FileBPlusTree[String, Any]( "btree", 4, true )
+	forAll (order4) { (gen, storage) =>
+		val t = gen()
 	
-		tree.prettyString shouldBe "[n0: (null, null, null)]"
-		tree.insert( "a" )
-		tree.insert( "b" )
-		tree.insert( "c" )
-		tree.insert( "d" )
-		tree.insert( "e" )
-		tree.insert( "f" )
-		tree.insert( "g" )
-		tree.insert( "h" )
-		tree.insert( "i" )
-		tree.insert( "j" )
-		tree.insert( "k" )
-		tree.insert( "l" )
-		tree.insert( "m" )
-		tree.insert( "n" )
-		tree.insert( "o" )
-		tree.insert( "p" )
-		tree.wellConstructed shouldBe "true"
-		tree.prettyString shouldBe
-			"""	|[n0: (null) n1 | g | n2 | m | n3]
-					|[n1: (n0) n4 | c | n5 | e | n6] [n2: (n0) n7 | i | n8 | k | n9] [n3: (n0) n10 | o | n11]
-					|[n4: (null, n1, n5) a b] [n5: (n4, n1, n6) c d] [n6: (n5, n1, n7) e f] [n7: (n6, n2, n8) g h] [n8: (n7, n2, n9) i j] [n9: (n8, n2, n10) k l] [n10: (n9, n3, n11) m n] [n11: (n10, n3, null) o p]""".stripMargin
-		tree.prettySearch( "h" ) shouldBe "n7 null 1"
+		"ascending insertion (order 4): " + storage in {
+			t.prettyString shouldBe "[n0: (null, null, null)]"
+			t.insert( "a" )
+			t.insert( "b" )
+			t.insert( "c" )
+			t.insert( "d" )
+			t.insert( "e" )
+			t.insert( "f" )
+			t.insert( "g" )
+			t.insert( "h" )
+			t.insert( "i" )
+			t.insert( "j" )
+			t.insert( "k" )
+			t.insert( "l" )
+			t.insert( "m" )
+			t.insert( "n" )
+			t.insert( "o" )
+			t.insert( "p" )
+			t.wellConstructed shouldBe "true"
+			t.prettyString shouldBe
+				"""	|[n0: (null) n1 | g | n2 | m | n3]
+						|[n1: (n0) n4 | c | n5 | e | n6] [n2: (n0) n7 | i | n8 | k | n9] [n3: (n0) n10 | o | n11]
+						|[n4: (null, n1, n5) a b] [n5: (n4, n1, n6) c d] [n6: (n5, n1, n7) e f] [n7: (n6, n2, n8) g h] [n8: (n7, n2, n9) i j] [n9: (n8, n2, n10) k l] [n10: (n9, n3, n11) m n] [n11: (n10, n3, null) o p]""".stripMargin
+			t.prettySearch( "h" ) shouldBe "n7 null 1"
+		}
 	}
 	
-	"descending insertion (order 4)" in {
-		val tree = new FileBPlusTree[String, Any]( "btree", 4, true )
+	forAll (order4) { (gen, storage) =>
+		val t = gen()
 	
-		tree.insertKeysAndCheck( "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a" ) shouldBe "true"
-		tree.prettyString shouldBe
-			"""	|[n0: (null) n1 | i | n2 | m | n3]
-					|[n1: (n0) n4 | c | n5 | e | n6 | g | n7] [n2: (n0) n8 | k | n9] [n3: (n0) n10 | o | n11]
-					|[n4: (null, n1, n5) a b] [n5: (n4, n1, n6) c d] [n6: (n5, n1, n7) e f] [n7: (n6, n1, n8) g h] [n8: (n7, n2, n9) i j] [n9: (n8, n2, n10) k l] [n10: (n9, n3, n11) m n] [n11: (n10, n3, null) o p]""".stripMargin
-		tree.prettySearch( "h" ) shouldBe "n7 null 1"
+		"descending insertion (order 4): " + storage in {
+			t.insertKeysAndCheck( "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a" ) shouldBe "true"
+			t.prettyString shouldBe
+				"""	|[n0: (null) n1 | i | n2 | m | n3]
+						|[n1: (n0) n4 | c | n5 | e | n6 | g | n7] [n2: (n0) n8 | k | n9] [n3: (n0) n10 | o | n11]
+						|[n4: (null, n1, n5) a b] [n5: (n4, n1, n6) c d] [n6: (n5, n1, n7) e f] [n7: (n6, n1, n8) g h] [n8: (n7, n2, n9) i j] [n9: (n8, n2, n10) k l] [n10: (n9, n3, n11) m n] [n11: (n10, n3, null) o p]""".stripMargin
+			t.prettySearch( "h" ) shouldBe "n7 null 1"
+		}
 	}
 	
-	"random insertion (order 4)" in	{
-		val tree = new FileBPlusTree[String, Any]( "btree", 4, true )
+	forAll (order4) { (gen, storage) =>
+		val t = gen()
 	
-		tree.insertKeysAndCheck( "v", "t", "u", "j", "g", "w", "y", "c", "n", "l", "a", "r", "b", "s", "e", "f", "i", "z", "h", "d", "p", "x", "m", "k", "o", "q" ) shouldBe "true"
-		tree.prettyString shouldBe
-			"""	|[n0: (null) n1 | h | n2 | n | n3 | u | n4]
-					|[n1: (n0) n5 | c | n6 | f | n7] [n2: (n0) n8 | j | n9 | l | n10] [n3: (n0) n11 | p | n12 | s | n13] [n4: (n0) n14 | w | n15 | y | n16]
-					|[n5: (null, n1, n6) a b] [n6: (n5, n1, n7) c d e] [n7: (n6, n1, n8) f g] [n8: (n7, n2, n9) h i] [n9: (n8, n2, n10) j k] [n10: (n9, n2, n11) l m] [n11: (n10, n3, n12) n o] [n12: (n11, n3, n13) p q r] [n13: (n12, n3, n14) s t] [n14: (n13, n4, n15) u v] [n15: (n14, n4, n16) w x] [n16: (n15, n4, null) y z]""".stripMargin
-		tree.prettySearch( "i" ) shouldBe "n8 null 1"
+		"random insertion (order 4): " + storage in	{
+			t.insertKeysAndCheck( "v", "t", "u", "j", "g", "w", "y", "c", "n", "l", "a", "r", "b", "s", "e", "f", "i", "z", "h", "d", "p", "x", "m", "k", "o", "q" ) shouldBe "true"
+			t.prettyString shouldBe
+				"""	|[n0: (null) n1 | h | n2 | n | n3 | u | n4]
+						|[n1: (n0) n5 | c | n6 | f | n7] [n2: (n0) n8 | j | n9 | l | n10] [n3: (n0) n11 | p | n12 | s | n13] [n4: (n0) n14 | w | n15 | y | n16]
+						|[n5: (null, n1, n6) a b] [n6: (n5, n1, n7) c d e] [n7: (n6, n1, n8) f g] [n8: (n7, n2, n9) h i] [n9: (n8, n2, n10) j k] [n10: (n9, n2, n11) l m] [n11: (n10, n3, n12) n o] [n12: (n11, n3, n13) p q r] [n13: (n12, n3, n14) s t] [n14: (n13, n4, n15) u v] [n15: (n14, n4, n16) w x] [n16: (n15, n4, null) y z]""".stripMargin
+			t.prettySearch( "i" ) shouldBe "n8 null 1"
+		}
 	}
-	
 }
