5276b315721466a4eb3ff3c704bea0739ed5f9c7
Edward A Maxedon, Sr <edadma@gmail.com>
2016-09-29 22:18:02 -0400

    scale back some more on slow tests

--- src/test/scala/StressTests.scala
+++ src/test/scala/StressTests.scala
@@ -17,11 +17,11 @@ class StressTests extends FreeSpec with PropertyChecks with Matchers {
 			(() => new FileBPlusTree[Int, Any]( newfile, 3 ),    "on disk", 3, 50, BasicTest),
 			(() => new FileBPlusTree[Int, Any]( newfile, 3 ),    "on disk", 3, 1000, SlowTest),
 			(() => new MemoryBPlusTree[Int, Any]( 4 ),    "in memory", 4, 1000, SlowTest),
-			(() => new FileBPlusTree[Int, Any]( newfile, 4 ),    "on disk", 4, 1000, SlowTest),
-			(() => new MemoryBPlusTree[Int, Any]( 5 ),    "in memory", 5, 1000, SlowTest),
-			(() => new FileBPlusTree[Int, Any]( newfile, 5 ),    "on disk", 5, 1000, SlowTest),
-			(() => new MemoryBPlusTree[Int, Any]( 6 ),    "in memory", 6, 1000, SlowTest),
-			(() => new FileBPlusTree[Int, Any]( newfile, 6 ),    "on disk", 6, 1000, SlowTest)
+			(() => new FileBPlusTree[Int, Any]( newfile, 4 ),    "on disk", 4, 1000, SlowTest)
+// 			(() => new MemoryBPlusTree[Int, Any]( 5 ),    "in memory", 5, 1000, SlowTest),
+// 			(() => new FileBPlusTree[Int, Any]( newfile, 5 ),    "on disk", 5, 1000, SlowTest),
+// 			(() => new MemoryBPlusTree[Int, Any]( 6 ),    "in memory", 6, 1000, SlowTest),
+// 			(() => new FileBPlusTree[Int, Any]( newfile, 6 ),    "on disk", 6, 1000, SlowTest)
 // 			(() => new MemoryBPlusTree[Int, Any]( 49 ),    "in memory", 49, 10000, SlowTest),
 // 			(() => new FileBPlusTree[Int, Any]( newfile, 49 ),    "on disk", 49, 10000, SlowTest),
 // 			(() => new MemoryBPlusTree[Int, Any]( 50 ),    "in memory", 50, 10000, SlowTest),
